5dc1e2b909771328d151d004b82e47ad29cf00f8
Priyank-a-a <43705382+Priyank-a-a@users.noreply.github.com>
2019-04-20 22:43:47 +0530

    Update Deadlock avoidance program.c

--- Deadlock avoidance program.c
+++ Deadlock avoidance program.c
@@ -3,7 +3,7 @@
 int current[5][5], maximum_claim[5][5], available[5];
 int allocation[5] = {0, 0, 0, 0, 0};
 int maxres[5], running[5], safe = 0;
-int counter = 0, i, j, resources, processes, k = 1;
+int counter = 0, i, j,exec, resources, processes, k = 1;
  
 int main()
 {
@@ -141,7 +141,7 @@ int main()
 		        printf("\n");
         	}
     	}
-    	return 0;
+   
 }
     	return 0;
 }
