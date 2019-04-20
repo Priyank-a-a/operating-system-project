0973597d77b56b664038006463f996f23c0b2dca
Priyank-a-a <43705382+Priyank-a-a@users.noreply.github.com>
2019-04-20 22:39:17 +0530

    Update Deadlock avoidance program.c

--- Deadlock avoidance program.c
+++ Deadlock avoidance program.c
@@ -108,6 +108,40 @@ int main()
                         			break;
                     			}
                 		}
-    	
+    	if (exec) 
+				{
+                    			printf("\nProcess%d is executing\n", i + 1);
+                    			running[i] = 0;
+                    			counter--;
+                    			safe = 1;
+ 
+                    			for (j = 0; j < resources; j++) 
+					{
+                        			available[j] += current[i][j];
+                    			}
+			                break;
+                		}
+            		}
+        	}
+        	if (!safe) 
+		{
+            		printf("\nThe processes are in unsafe state.\n");
+            		break;
+        	} 
+		else 
+		{
+            		printf("\nThe process is in safe state");
+            		printf("\nAvailable vector:");
+ 
+            		for (i = 0; i < resources; i++) 
+			{
+                		printf("\t%d", available[i]);
+            		}
+ 
+		        printf("\n");
+        	}
+    	}
+    	return 0;
+}
     	return 0;
 }
